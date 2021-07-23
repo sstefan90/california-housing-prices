108a32b208f716d0e0309c597bf6b8145412ff7b
Stephone Christian <stephone_christian@Stephones-MacBook-Pro-3.local>
2021-07-23 16:34:32 -0700

    rough project structure and data uploaded

---
+++ linalg-approach.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+'''
+let's take a look at developing linear regression models using stochastic gradient decent,
+batch gradient decent, and mini batch gradient decent
+
+we'll generate a plot that shows the training error (RMSE) at every learning step the algorithm takes
+find those in the plot folder!
+'''
